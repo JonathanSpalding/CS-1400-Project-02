aeb98c4d54cbaf70921c93ad903d839fe9aaa151
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-30 10:13:02 -0600

    CS 1400 C#

---
+++ Project02/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Project02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.splitTheGold = new System.Windows.Forms.Button();
+            this.numOfPiratesInput = new System.Windows.Forms.TextBox();
+            this.numOfGold = new System.Windows.Forms.TextBox();
+            this.jacksShare = new System.Windows.Forms.TextBox();
+            this.matesShare = new System.Windows.Forms.TextBox();
+            this.crewMembersShare = new System.Windows.Forms.TextBox();
+            this.piratesBenevolentFund = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 11.14286F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Of Pirates: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 11.14286F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number Of Gold Coins:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 11.14286F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(117, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jack\'s Share:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 11.14286F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(117, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mate\'s Share:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 11.14286F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(117, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(225, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Crew Member\'s Share:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 11.14286F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(117, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(263, 33);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Pirate\'s Benevolent Fund:";
+            // 
+            // splitTheGold
+            // 
+            this.splitTheGold.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splitTheGold.Location = new System.Drawing.Point(197, 173);
+            this.splitTheGold.Name = "splitTheGold";
+            this.splitTheGold.Size = new System.Drawing.Size(211, 47);
+            this.splitTheGold.TabIndex = 6;
+            this.splitTheGold.Text = "Split The Gold";
+            this.splitTheGold.UseVisualStyleBackColor = true;
+            this.splitTheGold.Click += new System.EventHandler(this.splitTheGold_Click);
+            // 
+            // numOfPiratesInput
+            // 
+            this.numOfPiratesInput.Location = new System.Drawing.Point(386, 61);
+            this.numOfPiratesInput.Name = "numOfPiratesInput";
+            this.numOfPiratesInput.Size = new System.Drawing.Size(100, 29);
+            this.numOfPiratesInput.TabIndex = 8;
+            // 
+            // numOfGold
+            // 
+            this.numOfGold.Location = new System.Drawing.Point(386, 109);
+            this.numOfGold.Name = "numOfGold";
+            this.numOfGold.Size = new System.Drawing.Size(100, 29);
+            this.numOfGold.TabIndex = 9;
+            // 
+            // jacksShare
+            // 
+            this.jacksShare.Location = new System.Drawing.Point(386, 245);
+            this.jacksShare.Name = "jacksShare";
+            this.jacksShare.Size = new System.Drawing.Size(100, 29);
+            this.jacksShare.TabIndex = 10;
+            // 
+            // matesShare
+            // 
+            this.matesShare.Location = new System.Drawing.Point(386, 280);
+            this.matesShare.Name = "matesShare";
+            this.matesShare.Size = new System.Drawing.Size(100, 29);
+            this.matesShare.TabIndex = 11;
+            // 
+            // crewMembersShare
+            // 
+            this.crewMembersShare.Location = new System.Drawing.Point(386, 315);
+            this.crewMembersShare.Name = "crewMembersShare";
+            this.crewMembersShare.Size = new System.Drawing.Size(100, 29);
+            this.crewMembersShare.TabIndex = 12;
+            // 
+            // piratesBenevolentFund
+            // 
+            this.piratesBenevolentFund.Location = new System.Drawing.Point(386, 350);
+            this.piratesBenevolentFund.Name = "piratesBenevolentFund";
+            this.piratesBenevolentFund.Size = new System.Drawing.Size(100, 29);
+            this.piratesBenevolentFund.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project02.Properties.Resources.crane_de_pirate;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 399);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 1000);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(612, 38);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.Color.Silver;
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(58, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(240, 34);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(612, 944);
+            this.Controls.Add(this.piratesBenevolentFund);
+            this.Controls.Add(this.crewMembersShare);
+            this.Controls.Add(this.matesShare);
+            this.Controls.Add(this.jacksShare);
+            this.Controls.Add(this.numOfGold);
+            this.Controls.Add(this.numOfPiratesInput);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.splitTheGold);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button splitTheGold;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox numOfPiratesInput;
+        private System.Windows.Forms.TextBox numOfGold;
+        private System.Windows.Forms.TextBox jacksShare;
+        private System.Windows.Forms.TextBox matesShare;
+        private System.Windows.Forms.TextBox crewMembersShare;
+        private System.Windows.Forms.TextBox piratesBenevolentFund;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Project02/Form1.cs
@@ -0,0 +1,80 @@
+﻿// Author: Jonathan Spalding
+// Assignment: Project 2
+// Instructor "Roger deBry
+// Clas: CS 1400
+// Date Written: 1/31/2016
+//
+// "I declare that the following source code was written solely by me. I understand that copying any source code, in whole or in part, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy."
+//----------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The splitTheGold_Click Method
+        // Purpose: give 12% of gold to Jack, 8% to his first mate, and 
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void splitTheGold_Click(object sender, EventArgs e)
+        {
+            //State any constants
+            const double JACKSTASH = .12;
+            const double MATESHARE = .08;
+            //convert input for both crew members and gold to double
+            double crew = double.Parse(numOfPiratesInput.Text);
+            double gold = double.Parse(numOfGold.Text);
+            //Hand out 3 coins to each member so they can party.
+            gold = gold - (crew - 2) * 3;
+            //find 12% of gold for jack
+            double jack = Math.Floor(gold * JACKSTASH);
+            gold = gold - jack;
+            //then find 8% of the remaining gold for is first Mate.
+            double mate = Math.Floor(gold * MATESHARE);
+            gold = gold - mate;
+            //divide remaining gold by number of crew members
+            double finalShare = Math.Floor(gold / crew);
+            //find the modulus to put the remainder in the Pirate's Benevolent Association
+            double leftOvers = gold % crew;
+            //convert totals into strings
+            string jackTotal = $"{jack}";
+            string mateTotal = $"{mate}";
+            string crewMemberTotal = $"{finalShare}";
+            string benevolentFundTotal = $"{leftOvers}";
+            //output in each of the output boxes
+            jacksShare.Text = jackTotal;
+            matesShare.Text = mateTotal;
+            crewMembersShare.Text = crewMemberTotal;
+            piratesBenevolentFund.Text = benevolentFundTotal;
+        }
+
+        // The exitToolStripMenuItem1_Click Method
+        // Purpose: Display a pop up box when you click About
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // The aboutToolStripMenuItem_Click Method
+        // Purpose: Display a pop up box when you click About
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jonathan Spalding\nCS1400\nProject 2");
+        }
+    }
+}
